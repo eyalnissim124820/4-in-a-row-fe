89d48fe8681a70d1f781aa1bd5c52fb1ddc9ee34
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-01-30 21:10:59 +0200

    Create game page

--- src/pages/createGame/CreateGamePage.js
+++ src/pages/createGame/CreateGamePage.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './CreateGamePage.css'
+import logo from '../../attachments/favicon_io (4)/4inARow 1.svg'
+import coin_y from '../../attachments/boardTools/Coin-yellow.svg'
+import coin_r from '../../attachments/boardTools/Coin-red.svg'
 
 export default function CreateGamePage() {
+
+  const [gameRoom, setGameRoom] = useState()
+
+  useEffect(() => {
+    const url = new URLSearchParams(window.location.search);
+    const room_id = url.get('id');
+    setGameRoom(room_id)
+  }, [])
+
   return (
-    <div>CreateGamePage</div>
+    <div className='createGamePage'>
+      <div className='gameCode-container'>
+        <img id='smallLogo' src={logo} alt='logo' />
+        <h6>Game Code</h6>
+        <p>1146</p>
+      </div>
+      <div className='loadingGame-container'>
+        <p>Waiting for the other player...</p>
+        <div className='loadingCoins'>
+          <img id='coin_y' src={coin_y} alt='coin_y' />
+          <img id='coin_r' src={coin_r} alt='coin_r' />
+        </div>
+      </div>
+    </div>
   )
 }
