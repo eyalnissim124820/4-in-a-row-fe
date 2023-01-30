89eb3bce7ef068f3ea2c6e3c2e02fa1f48829d6b
Renzo Vargas <renzovargas172@gmail.com>
2023-01-30 15:18:48 +0200

    app.js, authContext.js update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import SignupForm from './components/forms/signup/SignupForm';
 import useAuthContext from './hooks/useAuthContext';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   const { currentUser } = useAuthContext()
-
+  
   return (
-    <div>hello</div>
+    <BrowserRouter>    
+    <Routes>
+      <Route path='/loginPage' />
+      <Route path='/signupPage' />
+      <Route path='/homePage' />
+      <Route path='/gamePage' />
+      <Route path='/scoresPage' />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useState } from "react";
 
 export const Context = createContext();
@@ -6,29 +7,41 @@ const AuthContext = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(() => {
     return JSON.parse(localStorage.getItem("currentUser") || null);
   });
-
-  //users/signup’ [POST]
-  const signup = async () => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  //‘users/login’ [POST]
-  const login = async () => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
+  const [authError, setAuthError] = useState(null)
   const handleCurrentUser = (value) => {
     setCurrentUser(value);
   };
+  const handleAuthError =(value)=>{
+    setAuthError(value)
+  }
+
+  //users/signup’ [POST]
+  const signup = async (signupInfo) => {
+    try {
+    } catch (error) {
+      const res = await axios.post('http://localhost:8080', signupInfo);
+      return res;
+    }
+  };
+  //‘users/login’ [POST]
+  const login = async (loginInfo) => {
+    try {
+      const res = await axios.post('http://localhost:8080', loginInfo)
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
 
   const values = {
     currentUser,
+    authError,
     handleCurrentUser,
+    signup,
+    login,
+    handleAuthError
   };
 
   return <Context.Provider value={values}>{children}</Context.Provider>;
