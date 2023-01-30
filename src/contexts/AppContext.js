9b2b2fcaa49f3f342a7f6c14ceae80d64228b922
David <susuvnkl6@gmail.com>
2023-01-30 11:47:08 +0200

    first push

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,13 +1,47 @@
 import { createContext } from "react";
+import axios from "axios";
 
-export const Context = createContext()
+export const Context = createContext();
 
-const AppContext = ({children})=>{
+const AppContext = ({ children }) => {
+  //‘/scores [POST]  Add Score API
+  const addMatchScore = async () => {
+    try {
+      const res = axios;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  return(
-    <Context.Provider>
-      {children}
-    </Context.Provider>
-  )
-}
+  // ‘/scores [GET] (Protected to user only) Get All Users Score History API
+  const getScoreHistory = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  // ‘/scores/:id [GET] (Protected to user only) Get Logged In User’s Score history API
+  const getUserScoreHistory = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //‘/scores/last/:id [GET] (Protected to user only) Get user last score API
+  const getUserLastMatchScore = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //Get user highest score API ‘/scores/winningStreak/:id [GET] (Protected to user only)
+  const getUserWinningStreack = async () => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return <Context.Provider>{children}</Context.Provider>;
+};
 export default AppContext;
