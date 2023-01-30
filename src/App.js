376a7e9ebfb4ce6a10590c75415dc2cc32007525
Renzo Vargas <renzovargas172@gmail.com>
2023-01-30 15:18:48 +0200

    app.js, authContext.js update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
 import SignupForm from './components/forms/signup/SignupForm';
 import useAuthContext from './hooks/useAuthContext';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
 
   const { currentUser } = useAuthContext()
-
+  
   return (
-    // <div>hello</div>
-    <SignupForm />
+    <BrowserRouter>    
+      <Routes>
+        <Route path='/loginPage' />
+        <Route path='/signupPage' />
+        <Route path='/homePage' />
+        <Route path='/gamePage' />
+        <Route path='/scoresPage' />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
