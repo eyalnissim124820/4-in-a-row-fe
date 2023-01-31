d1d7efd19dcc4ded0e98f9ab83089d47ca31fb29
eyalnissim124820 <111303729+eyalnissim124820@users.noreply.github.com>
2023-01-31 15:51:11 +0200

    board game add coin

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Board.css'
+const matrix = [
+    [0, 0, 0, 0, 2, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]]
 
 export default function Board() {
-    
-  return (
-    <div>Board</div>
-  )
+
+
+    const [gameMatrix, setGameMatrix] = useState(matrix);
+
+
+    console.log(gameMatrix);
+
+
+    return (
+        <div className='board'>
+            <table>
+                {
+                    gameMatrix[0]?.map((column, i) => (
+                        <tr key={i}>
+                            {gameMatrix?.map((row, j) => (
+                                <td key={`${i},${j}`} id={`coin-${matrix[j][i]}`} onClick={() => {
+                                    const newMatrix = [...gameMatrix];
+                                    for (let slot = matrix[0].length; slot > 0; slot--) {
+                                        if (newMatrix[j][slot] === 0) {
+                                            newMatrix[j][slot] = 1;
+                                            setGameMatrix(newMatrix)
+                                            return
+                                        }
+                                    }
+
+                                }}></td>
+                            ))}
+                        </tr>
+                    ))
+                }
+            </table>
+        </div >
+    )
 }
