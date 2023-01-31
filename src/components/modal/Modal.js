3ffb16b8c66b6b1ffe9b669d1535817e5b5214ff
David <susuvnkl6@gmail.com>
2023-01-31 18:17:45 +0200

    modal design

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React from "react";
 
 export default function Modal({ children, setIsOpen }) {
+  const ModalStyle = {
+    position: "absolute",
+    right: "10%",
+    left: "10%",
+    top: "20%",
+    bottom: "20%",
+    backgroundColor: "white",
+    borderRadius: "20px",
+    zIndex: "4",
+    margin: "0",
+    padding: "0",
+    aspectRatio: "1 /1",
+  };
+  const ModalContainerStyle = {
+    backgroundColor: "rgba(0,0,0,0.44)",
+    position: "absolute",
+    width: "100vw",
+    height: "100vh",
+    top: "0",
+    right: "0",
+    zIndex: "3",
+  };
 
-
-    const ModalStyle = {
-        position: 'absolute',
-        right: '10%',
-        left: '10%',
-        top: '20%',
-        bottom: '20%',
-        backgroundColor: 'white',
-        borderRadius: '20px',
-        zIndex: '4',
-        margin: '0',
-        padding: '0'
-    }
-    const ModalContainerStyle = {
-        backgroundColor: "rgba(0,0,0,0.7)",
-        position: 'absolute',
-        width: "100vw",
-        height: "100vh",
-        top: '0',
-        right: '0',
-        zIndex: '3'
-    }
-
-    return (
-        <div onClick={() => setIsOpen(false)} style={ModalContainerStyle}>
-            <div className='modal' onClick={(e) => { e.stopPropagation(); }} style={ModalStyle}>
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div onClick={() => setIsOpen(false)} style={ModalContainerStyle}>
+      <div
+        className="modal"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        style={ModalStyle}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
-
-
--- src/pages/game/GamePage.js
+++ src/pages/game/GamePage.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react'
-import Board from '../../components/board/Board'
-import './GamePage.css'
-import Modal from '../../components/modal/Modal';
-import redCoin from '../../attachments/boardTools/Coin-red.svg'
-
-
+import React, { useState } from "react";
+import Board from "../../components/board/Board";
+import "./GamePage.css";
+import Modal from "../../components/modal/Modal";
+import redCoin from "../../attachments/boardTools/Coin-red.svg";
 
 export default function GamePage() {
-
-  const [modal, setModal] = useState(true)
+  const [modal, setModal] = useState(true);
 
   return (
-    <div className='gamePage-page'>
-      <div className='gamePage-header'>
-      </div>
-      <div className='gamePage-body'>
+    <div className="gamePage-page">
+      <div className="gamePage-header"></div>
+      <div className="gamePage-body">
         <Board />
       </div>
-      <div className='gamePage-footer'>
-        {modal
-          ?
+      <div className="gamePage-footer">
+        {modal ? (
           <Modal setIsOpen={setModal}>
-            <div className='welcome-container'>
-              <div>
-                <h6>congratulations </h6>
-                <h1 style={{ fontWeight: '100' }}><img src={redCoin} alt='redCoin' /></h1>
+            <div className="modal-container">
+              <div className="modal-headers-container">
+                <h1 style={{ fontWeight: "500" }}>congratulations </h1>
+                <h1 style={{ fontWeight: "200" }}>
+                  Y<img style={{ height: "18px" }} src={redCoin} alt="redCoin" />u Won!
+                </h1>
+              </div>
+              <div className="modal-buttons-container">
+                <button>Play again</button>
+                <button>Score</button>
               </div>
             </div>
           </Modal>
-          :
-          ''
-        }
+        ) : (
+          ""
+        )}
       </div>
     </div>
-  )
+  );
 }
