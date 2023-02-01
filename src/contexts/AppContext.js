594d3b63aafd0fe5d91d016e8225ddf4f0d00a9d
David <susuvnkl6@gmail.com>
2023-02-01 11:18:33 +0200

    connect all the request to the routs

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -4,18 +4,16 @@ import axios from "axios";
 export const Context = createContext();
 
 const AppContext = ({ children }) => {
-  const [matchDetails, setMatchDetails] = useState(null) 
+  const [matchDetails, setMatchDetails] = useState(null);
 
-  const handleMatchDetails =(value)=>{
-    setMatchDetails(value)
-  }
-
-  
+  const handleMatchDetails = (value) => {
+    setMatchDetails(value);
+  };
 
   //‘/scores [POST]  Add Score API
   const addMatchScore = async () => {
     try {
-      // const res = axios;
+      const res = axios.post();
     } catch (error) {
       console.log(error);
     }
@@ -24,54 +22,70 @@ const AppContext = ({ children }) => {
   // ‘/scores [GET] (Protected to user only) Get All Users Score History API
   const getScoreHistory = async () => {
     try {
+      const res = await axios.get("http://localhost:8080/users/topScores");
+      console.log(`getScoreHistory`, res.data);
     } catch (error) {
       console.log(error);
     }
   };
   // ‘/scores/:id [GET] (Protected to user only) Get Logged In User’s Score history API
-  const getUserScoreHistory = async () => {
+  const getUserScoreHistory = async (userId) => {
     try {
+      const res = await axios.get(`http://localhost:8080/matches/history/${userId}}`);
+      console.log(`getUserScoreHistory`, res.data);
     } catch (error) {
       console.log(error);
     }
   };
   //‘/scores/last/:id [GET] (Protected to user only) Get user last score API
-  const getUserLastMatchScore = async () => {
+  const getUserLastMatchScore = async (userId) => {
     try {
+      const res = await axios.get(`http://localhost:8080/matches/lastScore/${userId}`);
+      console.log(`getUserLastMatchScore`, res.data);
     } catch (error) {
       console.log(error);
     }
   };
   //Get user highest score API ‘/scores/winningStreak/:id [GET] (Protected to user only)
-  const getUserWinningStreack = async () => {
+  const getUserWinningStreack = async (userId) => {
     try {
+      const res = await axios.get(`http://localhost:8080/matches/longStrike/${userId}`);
+      console.log(`getUserWinningStreack`, res.data);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const createMatch = async (userId) =>{
-    try{
-      const response = await  axios.post('http://localhost:8080/game', userId)
-      const roomId = response.data.id
-      console.log(roomId)
-      return roomId
-    }catch(error){
-      console.log(error)
+  //////////// test ///////////
+  useEffect(() => {
+    getScoreHistory();
+    getUserScoreHistory(5);
+    getUserLastMatchScore(5);
+    getUserWinningStreack(5);
+  }, []);
+
+  //////////////////////////
+
+  const createMatch = async (userId) => {
+    try {
+      const response = await axios.post("http://localhost:8080/game", userId);
+      const roomId = response.data.id;
+      console.log(roomId);
+      return roomId;
+    } catch (error) {
+      console.log(error);
     }
-  }
-  const joinMatch = (userId, roomId) => {
+  };
+  const joinMatch = (userId, roomId) => {};
 
-  }
-
-  const values ={
+  const values = {
     addMatchScore,
     getScoreHistory,
     createMatch,
     joinMatch,
     handleMatchDetails,
-    matchDetails
-  }
+    matchDetails,
+  };
   return <Context.Provider value={values}>{children}</Context.Provider>;
 };
 export default AppContext;
